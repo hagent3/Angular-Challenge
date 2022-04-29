4cf87d986e478c08ff6a324440f0ca4ac152c7f9
hagent3 <artur.w.bauer@gmail.com>
2022-04-29 15:37:36 +0200

    feature(patient): adjustment

--- red-client-probeaufgabe-main/src/app/ui/fhir-info/components/patients/patients.component.ts
+++ red-client-probeaufgabe-main/src/app/ui/fhir-info/components/patients/patients.component.ts
@@ -1,15 +1,42 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
+import { PatientSearchService } from "../../../../search/services/patient-search.service";
+import {Observable, Subscription} from "rxjs";
+import { ActivatedRoute } from "@angular/router";
+import { IFhirPatient } from "@red-probeaufgabe/types";
 
 @Component({
   selector: 'app-patients',
   templateUrl: './patients.component.html',
   styleUrls: ['./patients.component.scss']
 })
-export class PatientsComponent implements OnInit {
+export class PatientsComponent implements OnInit, OnDestroy {
 
-  constructor() { }
+  loading = true;
+  onPatientSubscribtion: Subscription;
+  patient: Observable<IFhirPatient>;
+  constructor(private readonly patientSearchService: PatientSearchService,
+              private readonly activatedRouter: ActivatedRoute) { }
 
   ngOnInit(): void {
+
+    this.onPatientId();
   }
 
+  ngOnDestroy(): void {
+    if (this.onPatientSubscribtion) {
+      this.onPatientSubscribtion.unsubscribe();
+    }
+  }
+
+  private onPatientId() {
+
+    this.onPatientSubscribtion = this.activatedRouter.params.subscribe(async (params) => {
+      const id = params['id'];
+      if (id) {
+        this.loading = true;
+        this.patient = await this.patientSearchService.findById(id);
+      }
+    })
+
+  }
 }
